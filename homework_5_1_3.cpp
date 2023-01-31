683bd9eafe122ca9b52829866a73b5b3721e8894
Valentina Baranova <v.sinekopova@gmail.com>
2023-01-31 23:00:37 +0300

    Initial commit

---
+++ homework_5_1_3.cpp
@@ -0,0 +1,35 @@
+﻿#include <cmath>
+#include <iostream>
+#include <functional>
+#include <vector>
+
+int main()
+{
+	double angles[4]{ 30 * 3.1415926 / 180, 60 * 3.1415926 / 180, 90 * 3.1415926 / 180};
+	std::cout << "[Входные данные]: ";
+	for(const auto & angle : angles)
+		std::cout << angle * 180 / 3.1415926  << " * 3.1415926 / 180, ";
+	std::cout << "\n";
+
+	auto psin = [](double angle) {
+		std::cout << "sin : " << sin(angle) << " ";
+	};
+	auto pcos = [](double angle) {
+		std::cout << "cos : " << cos(angle) << " ";
+	};
+
+	std::vector<std::function<void(const double)>> functions = { psin, pcos };
+
+	//auto ptan = [](double angle) {
+	//	std::cout << "tan : " << tan(angle) << " ";
+	//};
+	//functions.push_back(ptan);
+
+	std::cout << "[Выходные данные] : \n";
+	for (const auto& angle : angles) {
+		std::cout << angle << ": ";
+		for (const auto& function : functions)
+			function(angle);
+		std::cout << std::endl;
+	}
+}
